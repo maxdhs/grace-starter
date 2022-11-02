7dfc25a32b525cbd23b3ee9d28ce8635d34a2506
maxwell dubin <maxdhs@gmail.com>
2022-11-01 20:20:44 -0400

    react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,31 @@
-import logo from "./logo.svg";
+import { useEffect, useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Books from "./components/Books";
 
 function App() {
-  return <div className="App">grace starter</div>;
+  const [books, setBooks] = useState([]);
+  // i want to fetch the actual books and save them in state
+  const fetchBooks = async () => {
+    const response = await fetch("/api/books");
+    const data = await response.json();
+    setBooks(data.books);
+  };
+
+  console.log(books);
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  return (
+    <div>
+      <Link to="/books">Books</Link>
+      <Routes>
+        <Route path="/books" element={<Books />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
