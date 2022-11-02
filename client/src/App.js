9ce811c3c67be0d2a78f49ab717ab6672e1f5267
maxdhs <43966656+maxdhs@users.noreply.github.com>
2022-11-01 20:20:26 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">grace starter</div>;
+  return <div className="App">grace starter!</div>;
 }
 
 export default App;
